426a22099d60825b119541d1d427b60c04d10de0
Matt Hoyle <matt@deployable.co>
2017-01-18 04:32:44 +0000

    Initial build from node-es2015 det template
    API Complete

---
+++ README.md
@@ -0,0 +1,47 @@
+# [deployable-ringbuffer](https://github.com/deployable/deployable-ringbuffer)
+
+Circular buffer stored in an array
+
+### Install
+ 
+    npm install deployable-ringbuffer --save
+
+    yarn add deployable-ringbuffer
+
+### Usage
+
+```javascript
+
+const RingBuffer = require('deployable-ringbuffer')
+const rb = new RingBuffer(5)
+
+// Add items
+rb.add('one')
+rb.add('two')
+rb.add('three')
+rb.add('four')
+rb.add('five')
+
+// Get the last item
+rb.last() // => 'five'
+
+// Get the entire array
+rb.toArray() // => [ 'one', 'two', 'three', 'four', 'five' ]
+
+// Oldest on the left will be overwritten
+rb.add('six')
+rb.toArray() // => [ 'two', 'three', 'four', 'five', 'six' ]
+
+// Clear the buffer down
+rb.clear()
+rb.toArray() // => [ , , , , ]
+
+```
+
+### License
+
+deployable-ringbuffer is released under the MIT license.
+Copyright 2016 Matt Hoyle <code at deployable.co>
+
+https://github.com/deployable/deployable-ringbuffer
+
---
+++ test/unit_package_spec.js
@@ -0,0 +1,82 @@
+const RingBuffer = require('../')
+
+describe('Unit::deployable-ringbuffer', function(){
+
+  describe('Something does something', function(){
+  
+    it('should create a RingBuffer', function(){
+      expect( new RingBuffer(5) ).to.be.ok
+    })
+
+    it('should get the last added value with no arg', function(){
+      let rb = new RingBuffer(4)
+      rb.add('zero')
+      rb.add('one')
+      expect( rb.last() ).to.equal('one')
+    })
+
+    it('should get the last added value on the boundary', function(){
+      let rb = new RingBuffer(3)
+      rb.add('zero')
+      rb.add('one')
+      rb.add('two')
+      expect( rb.last() ).to.equal('two')
+    })
+
+    it('should get the last added value over the boundary', function(){
+      let rb = new RingBuffer(2)
+      rb.add('zero')
+      rb.add('one')
+      rb.add('two')
+      expect( rb.last() ).to.equal('two')
+    })
+
+    it('should get undefined when there is no last', function(){
+      let rb = new RingBuffer(3)
+      expect( rb.last() ).to.equal( undefined )
+    })
+
+    it('should create an array when buffer not full', function(){
+      let rb = new RingBuffer(3)
+      rb.add('zero')
+      rb.add('one')
+      expect( rb.toArray() ).to.eql([ , 'zero','one' ])
+    })
+
+    it('should create an array when buffer full', function(){
+      let rb = new RingBuffer(3)
+      rb.add('zero')
+      rb.add('one')
+      rb.add('two')
+      expect( rb.toArray() ).to.eql(['zero','one', 'two'])
+    })
+
+    it('should create an array when buffer is over', function(){
+      let rb = new RingBuffer(3)
+      rb.add('zero')
+      rb.add('one')
+      rb.add('two')
+      rb.add('three')
+      rb.add('four')
+      expect( rb.toArray() ).to.eql(['two','three','four'])
+    })
+
+    it('should clear a buffer', function(){
+      let rb = new RingBuffer(2)
+      rb.add('zero')
+      rb.add('one')
+      rb.clear()
+      expect( rb.last() ).to.be.undefined
+      expect( rb.toArray() ).to.eql([,,])
+    })
+
+    it('should return a write stream', function(){
+      let rb = new RingBuffer(2)
+      let ws = rb.writeStream()
+      ws.write('test')
+      expect( rb.last().toString() ).to.equal( 'test' )
+    })
+
+  })
+
+})
